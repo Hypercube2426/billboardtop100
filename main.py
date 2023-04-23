db18348327a6212085ca7e00e4c3b299c9dd44fa
Dhruv  Bhasin <dhruvbhasin0007@gmail.com>
2023-04-23 15:40:16 +0530

    third commit

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello")
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://www.billboard.com/charts/hot-100/2000-08-12/")
+bill_board = response.text
+soup = BeautifulSoup(bill_board, 'html.parser')
+print(soup.prettify())
